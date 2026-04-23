7d52df56d791ee9a359c00a0a2a81f9069c04d31
bashz <bechir.nemlaghi@solytic.com>
2026-04-23 10:12:53 +0100

    new vue project - with demo, oxfm, ts, vitest, router, store

---
+++ src/views/AboutView.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { findUsersById } from '@/api'
+
+onMounted(async () => {
+  const user5 = await findUsersById('5')
+  console.log(user5)
+})
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
