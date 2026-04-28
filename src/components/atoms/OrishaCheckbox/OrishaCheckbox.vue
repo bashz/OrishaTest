bd1025b89635113046f72731d4f07db59eed1fee
bashz <bechir.nemlaghi@solytic.com>
2026-04-28 12:03:13 +0100

    component: checkbox

--- src/components/atoms/OrishaCheckbox/OrishaCheckbox.vue
+++ src/components/atoms/OrishaCheckbox/OrishaCheckbox.vue
@@ -1,27 +1,84 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed } from 'vue'
 
-const title = ref('Change Me')
+interface Props {
+  label: string
+  disabled?: boolean
+}
+
+const model = defineModel()
+const props = defineProps<Props>()
+
+const classes = computed(() => ({
+  'orisha-a-checkbox--active': model.value,
+  'orisha-a-checkbox--disabled': props.disabled,
+}))
 </script>
 
 <template>
-  <div class="orisha-a-checkbox">
-    <h1 class="orisha-a-checkbox__title">{{ title }}</h1>
+  <div class="orisha-a-checkbox" :class="classes">
+    <div class="orisha-a-checkbox__box">
+      <input type="checkbox" v-model="model" :disabled="disabled" />
+      <i class="ph-light ph-check" v-if="model"></i>
+      <i class="ph-bold" v-else></i>
+    </div>
+    <div v-if="label" class="orisha-a-checkbox__label">{{ label }}</div>
   </div>
 </template>
 
 <style lang="scss">
 @use './variables' as checkbox;
+@use '@/assets/scss/variables/corners' as corners;
+@use '@/assets/scss/variables/colors' as colors;
+@use '@/assets/scss/mixins/box' as box;
 
 .orisha-a-checkbox {
-  /* Add component styles here */
-  @media (prefers-color-scheme: dark) {
+  @include box.flex;
+
+  &__box {
+    width: checkbox.$size;
+    height: checkbox.$size;
+    border-width: 1px;
+    border-style: solid;
+    border-color: colors.$stroke-primary;
+    border-radius: corners.$steep;
+    background-color: colors.$surface;
+    opacity: 1;
+
+    input {
+      width: checkbox.$size;
+      height: checkbox.$size;
+      position: absolute;
+      margin: 0;
+      cursor: pointer;
+      opacity: 0;
+    }
+
+    i {
+      font-size: 16px;
+      color: colors.$text-invert;
+      padding: 1px;
+    }
   }
 
-  @media (hover: hover) {
+  &__label {
+    margin-left: 4px;
+  }
+}
+
+.orisha-a-checkbox--active {
+  .orisha-a-checkbox__box {
+    background-color: colors.$primary;
+  }
+}
+
+.orisha-a-checkbox--disabled {
+  .orisha-a-checkbox__box {
+    opacity: 0.4;
   }
 
-  @media (min-width: 1024px) {
+  input {
+    cursor: initial;
   }
 }
 </style>
--- src/components/atoms/OrishaCheckbox/__stories__/OrishaCheckbox.story.vue
+++ src/components/atoms/OrishaCheckbox/__stories__/OrishaCheckbox.story.vue
@@ -1,12 +1,18 @@
 <script lang="ts" setup>
+import { reactive, ref } from 'vue'
 import OrishaCheckbox from '../OrishaCheckbox.vue'
+
+const isChecked = ref(false)
+const state = reactive({
+  label: 'Label',
+  disabled: false,
+})
 </script>
 
 <template>
   <Story group="atoms" title="Checkbox">
-    <Variant>
-      <OrishaCheckbox/>
+    <Variant title="checkbox">
+      <OrishaCheckbox :label="state.label" v-model="isChecked" :disabled="state.disabled" />
     </Variant>
   </Story>
 </template>
-  
