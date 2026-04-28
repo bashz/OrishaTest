1e9a89acce592de8b0189d9286ac9db8b2c2bf3c
bashz <bechir.nemlaghi@solytic.com>
2026-04-28 12:03:32 +0100

    component: radio button

--- src/components/atoms/OrishaRadio/OrishaRadio.vue
+++ src/components/atoms/OrishaRadio/OrishaRadio.vue
@@ -1,27 +1,101 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed } from 'vue'
 
-const title = ref('Change Me')
+interface Props {
+  name: string
+  value: string
+  disabled: string
+}
+
+const model = defineModel()
+const props = defineProps<Props>()
+
+const classes = computed(() => ({
+  'orisha-a-radio--active': model.value === props.value,
+  'orisha-a-radio--disabled': props.disabled,
+}))
 </script>
 
 <template>
-  <div class="orisha-a-radio">
-    <h1 class="orisha-a-radio__title">{{ title }}</h1>
+  <div class="orisha-a-radio" :class="classes">
+    <div class="orisha-a-radio__box">
+      <input type="radio" :name="name" :value="value" v-model="model" :disabled="disabled" />
+      <i class="ph-light ph-check" v-if="model === value"></i>
+      <i class="ph-bold" v-else></i>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
 @use './variables' as radio;
+@use '@/assets/scss/variables/corners' as corners;
+@use '@/assets/scss/variables/colors' as colors;
+@use '@/assets/scss/mixins/box' as box;
 
 .orisha-a-radio {
-  /* Add component styles here */
-  @media (prefers-color-scheme: dark) {
+  width: radio.$size;
+  height: radio.$size;
+  border-width: 1px;
+  border-style: solid;
+  border-color: colors.$stroke-primary;
+  border-radius: corners.$circle;
+  background-color: colors.$surface;
+  opacity: 1;
+
+  &__box {
+    @include box.flex;
+    width: radio.$circle-size;
+    height: radio.$circle-size;
+    border-radius: corners.$circle;
+    margin: 1px;
   }
 
-  @media (hover: hover) {
+  input {
+    width: radio.$size;
+    height: radio.$size;
+    position: absolute;
+    margin: 0;
+    cursor: pointer;
+    opacity: 0;
   }
 
-  @media (min-width: 1024px) {
+  i {
+    font-size: 13px;
+    color: colors.$text-invert;
+    padding: 2px;
+  }
+}
+
+.orisha-a-radio--sm {
+  width: radio.$size-sm;
+  height: radio.$size-sm;
+
+  .orisha-a-radio__box {
+    width: radio.$circle-size-sm;
+    height: radio.$circle-size-sm;
+  }
+
+  input {
+    width: radio.$size-sm;
+    height: radio.$size-sm;
+  }
+
+  i {
+    font-size: 11px;
+  }
+}
+
+.orisha-a-radio--active {
+  .orisha-a-radio__box {
+    background-color: colors.$primary;
+  }
+}
+
+.orisha-a-radio--disabled {
+  opacity: 0.4;
+
+  input {
+    cursor: initial;
   }
 }
 </style>
--- src/components/atoms/OrishaRadio/__stories__/OrishaRadio.story.vue
+++ src/components/atoms/OrishaRadio/__stories__/OrishaRadio.story.vue
@@ -1,12 +1,52 @@
 <script lang="ts" setup>
+import { reactive, ref } from 'vue'
 import OrishaRadio from '../OrishaRadio.vue'
+
+const value = ref('')
+const disabled = ref(false)
+const state1 = reactive({
+  name: 'radio',
+  value: 'value1',
+})
+const state2 = reactive({
+  name: 'radio',
+  value: 'value2',
+})
+const state3 = reactive({
+  name: 'radio',
+  value: 'value3',
+})
 </script>
 
 <template>
   <Story group="atoms" title="Radio">
-    <Variant>
-      <OrishaRadio/>
+    <Variant title="Medium">
+      <OrishaRadio :name="state1.name" :value="state1.value" v-model="value" :disabled="disabled" />
+      <OrishaRadio :name="state2.name" :value="state2.value" v-model="value" :disabled="disabled" />
+      <OrishaRadio :name="state3.name" :value="state3.value" v-model="value" :disabled="disabled" />
+    </Variant>
+    <Variant title="Small">
+      <OrishaRadio
+        class="orisha-a-radio--sm"
+        :name="state1.name"
+        :value="state1.value"
+        v-model="value"
+        :disabled="disabled"
+      />
+      <OrishaRadio
+        class="orisha-a-radio--sm"
+        :name="state2.name"
+        :value="state2.value"
+        v-model="value"
+        :disabled="disabled"
+      />
+      <OrishaRadio
+        class="orisha-a-radio--sm"
+        :name="state3.name"
+        :value="state3.value"
+        v-model="value"
+        :disabled="disabled"
+      />
     </Variant>
   </Story>
 </template>
-  
